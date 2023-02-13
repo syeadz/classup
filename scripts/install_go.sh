05ec449a349926b7ff9533ed00dc8e7aa67db724
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-02-13 13:28:36 -0500

    feat(scripts): created GO installation script

---
+++ scripts/install_go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EXPORT="Now you need to add export PATH=%\$PATH:/usr/local/go/bin to .bashrc"
+NAME="go1.20.linux-amd64.tar.gz"
+
+if [ -x "$(command -v go)" ]; then
+  echo 'GO is already installed'
+  exit
+fi
+
+/usr/local/go/bin
+if [ -f "/usr/local/go/bin" ]; then
+    echo $EXPORT
+else
+    wget -O /tmp/$NAME https://go.dev/dl/$NAME
+    tar -C /usr/local -xzf /tmp/$NAME
+    echo "GO installed successfully"
+fi
+
+echo $EXPORT
