d1700e2a93ee2178adab803f1d91401a77d473c0
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-02-14 20:57:08 -0500

    fix(scripts): changed quote url to different one

--- scripts/quotes.sh
+++ scripts/quotes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# FILE=/etc/classup/quotes.csv
-FILE=/home/syead/info/quotes.csv
+URL=https://gist.githubusercontent.com/JakubPetriska/060958fd744ca34f099e947cd080b540/raw/963b5a9355f04741239407320ac973a6096cd7b6/quotes.csv
+FILE=~/.classup/quotes.csv
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE already exists."
     exit
 fi
 
@@ -11,10 +11,10 @@ if [ "$EUID" -ne 0 ]
   then echo "error: not root (use sudo)" >&2 ; exit
 fi
 
-curl https://raw.githubusercontent.com/akhiltak/inspirational-quotes/master/Quotes.csv --create-dirs -o $FILE
+curl $URL --create-dirs -o $FILE
 
 if [ -f "$FILE" ]; then
-    echo "Successfully downloaded quotes.csv at /etc/classup/"
+    echo "Successfully downloaded quotes.csv at $FILE"
     exit
 else
     echo "Error: file wasn't created" >&2 ; exit
